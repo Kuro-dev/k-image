b396acfb743ea7aa7efbccfddad6091a87d59fe6
kuro-dev <dev_kuro@yahoo.com>
2024-06-26 15:08:16 +0200

    removed test that didn't work

--- src/test/java/org/kurodev/kimage/kimage/font/GeneralFontTests.java
+++ src/test/java/org/kurodev/kimage/kimage/font/GeneralFontTests.java
@@ -1,9 +1,8 @@
 package org.kurodev.kimage.kimage.font;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.kurodev.kimage.kimage.font.glyph.simple.Coordinate;
 import org.kurodev.kimage.kimage.font.glyph.FontGlyph;
+import org.kurodev.kimage.kimage.font.glyph.simple.Coordinate;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,57 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.kurodev.kimage.kimage.font.Helper.coordinatesForLetterA;
 
 public class GeneralFontTests {
-
-    private static final LetterEntry[] letters = {
-            new LetterEntry(' ', 3, 320),
-            new LetterEntry('A', 4, 448),
-            new LetterEntry('B', 5, 448),
-            new LetterEntry('C', 6, 448),
-            new LetterEntry('D', 7, 448),
-            new LetterEntry('E', 8, 448),
-            new LetterEntry('F', 9, 448),
-            new LetterEntry('G', 10, 448),
-            new LetterEntry('H', 11, 448),
-            new LetterEntry('I', 12, 448),
-            new LetterEntry('J', 13, 448),
-            new LetterEntry('K', 14, 512),
-            new LetterEntry('L', 15, 448),
-            new LetterEntry('M', 16, 448),
-            new LetterEntry('N', 17, 832),
-            new LetterEntry('O', 18, 512),
-            new LetterEntry('P', 19, 448),
-            new LetterEntry('Q', 20, 576),
-            new LetterEntry('R', 21, 448),
-            new LetterEntry('S', 22, 448),
-            new LetterEntry('T', 23, 448),
-            new LetterEntry('U', 24, 448),
-            new LetterEntry('V', 25, 448),
-            new LetterEntry('W', 26, 768),
-            new LetterEntry('X', 27, 448),
-            new LetterEntry('Y', 28, 448),
-            new LetterEntry('Z', 29, 448),
-    };
-
-    private static FontReader font;
-
-    @BeforeAll
-    public static void prepare() throws IOException {
-        font = (FontReader) FontReaders.loadFont(FontReaders.class.getResourceAsStream("/kimage/Pixelletters.ttf"));
-    }
-
-    @Test
-    public void testAdvanceWidth() {
-        //find correct index for each character in the font
-        for (LetterEntry letter : letters) {
-            int index = font.getGlyphIndex(letter.character);
-            assertEquals(letter.index, index, "Index is wrong: " + letter);
-        }
-        //Check if advancewidth for every letter is as expected
-        for (LetterEntry letter : letters) {
-            int index = font.getGlyphIndex(letter.character);
-            assertEquals(letter.advanceWidth, font.getAdvanceWidth(index), "Advancewidth is wrong " + letter);
-        }
-    }
 
     @Test
     public void testBasicFontData() throws IOException {
